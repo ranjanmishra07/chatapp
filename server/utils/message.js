dd3a80bd4effa7a58d77c003516cfb894ceb1e24
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-01-24 19:28:07 +0530

    ädded moment for timestamp

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,14 +1,16 @@
+const moment=require('moment');
 var generateMessage=function(from,text){
   return {
     from,
     text,
-    createdAt:new Date().getTime()
+    createdAt:moment().valueOf()
   }
 }
 var generateLocationMessage=function(from,latitude,longitude){
   return {
     from,
-    url:`https://www.google.com/maps?q=${latitude},${longitude}`
+    url:`https://www.google.com/maps?q=${latitude},${longitude}`,
+    createdAt:moment().valueOf()
   }
 }
 module.exports={generateMessage,generateLocationMessage};
