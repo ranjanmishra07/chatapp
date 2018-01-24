96e8e6ee711399749306633ab6b92ea8bb2677ac
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-01-24 15:35:51 +0530

    added jquery "

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http=require('http');
 const express=require('express');
 const publicPath=path.join(__dirname,'../public');
 const socketIO=require('socket.io');
+const {generateMessage}=require('./utils/message');
 var app=express();
 const port=process.env.PORT||3000;
 var server=http.createServer(app);
@@ -14,29 +15,23 @@ io.on('connection',(socket)=>{
   console.log('user connected');
 
 
-  socket.emit('newMessage',{
-    from:'admin',
-    text:'welcome new user',
-    createdAt:new Date().getTime()
-  })
-  socket.broadcast.emit('newMessage',{
-    from:'admin',
-    text:'new user connected',
-    createdAt:new Date().getTime()
-  })
+  socket.emit('newMessage',generateMessage('Admin','welcome to the chat app'))
+  socket.broadcast.emit('newMessage',generateMessage('Admin','new user conected'));
+
   socket.on('createMessage',(message)=>{
     console.log('creating a message from client',message);
-    io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createdAt:new Date().getTime()
-    })
+    io.emit('newMessage',generateMessage(message.from,message.text))
     // socket.broadcast.emit('newMessage',{
     //   from:message.from,
     //   text:message.text,
     //   createdAt:new Date().getTime()
     // })
   })
+
+  socket.on('createLocationMessage',(coords)=>{
+    io.emit('newMessage',generateMessage('Admin',`${coords.latitude},${coords.longitude}`));
+  })
+
   socket.on('disconnect',()=>{
     console.log('client disconnected');
   });
